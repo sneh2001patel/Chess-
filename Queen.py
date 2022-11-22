796e812f61288d893aae362d4a5b049753cf3efc
Sneh Patel <snehpatel@MacBook-Pro-163.local>
2022-11-22 17:11:41 -0500

    Got valid moves for the queen

--- Queen.py
+++ Queen.py
@@ -22,3 +22,89 @@ class Queen(pg.sprite.Sprite):
 
     def update(self):
         self.rect.center = (self.x, self.y)
+
+    def valid_moves(self):
+        moves = []
+        print("Current: ", self.game.board[self.pos[1]][self.pos[0]])
+        print("Up-right: ", self.game.board[self.pos[1] - 1][self.pos[0]])
+        print("Down: ", self.game.board[self.pos[1] + 1][self.pos[0]])
+        print("Right: ", self.game.board[self.pos[1]][self.pos[0] + 1])
+        print("Left: ", self.game.board[self.pos[1]][self.pos[0] - 1])
+
+        # Up-right directions
+        i = 1
+        while True:
+            if (self.pos[1] - i >= 0) and (self.pos[0] + i <= 7) and (
+                    self.game.board[self.pos[1] - i][self.pos[0] + i] == ""):
+                moves.append([self.pos[0] + i, self.pos[1] - i])
+                i += 1
+            else:
+                break
+
+        # Up-Left directions
+        j = 1
+        while True:
+            if (self.pos[1] - j >= 0) and (self.pos[0] - j >= 0) and (
+                    self.game.board[self.pos[1] - j][self.pos[0] - j] == ""):
+                moves.append([self.pos[0] - j, self.pos[1] - j])
+                j += 1
+            else:
+                break
+
+        # down-right directions
+        k = 1
+        while True:
+            if (self.pos[1] + k <= 7) and (self.pos[0] + k <= 7) and (
+                    self.game.board[self.pos[1] + k][self.pos[0] + k] == ""):
+                moves.append([self.pos[0] + k, self.pos[1] + k])
+                k += 1
+            else:
+                break
+
+        # down-right directions
+        m = 1
+        while True:
+            if (self.pos[1] + m <= 7) and (self.pos[0] - m >= 0) and (
+                    self.game.board[self.pos[1] + m][self.pos[0] - m] == ""):
+                moves.append([self.pos[0] - m, self.pos[1] + m])
+                m += 1
+            else:
+                break
+            # Up directions
+        i = 1
+        while True:
+            if (self.pos[1] - i >= 0) and (self.game.board[self.pos[1] - i][self.pos[0]] == ""):
+                moves.append([self.pos[0], self.pos[1] - i])
+                i += 1
+            else:
+                break
+
+        # Down Direction
+        j = 1
+        while True:
+            if (self.pos[1] + j <= 7) and (self.game.board[self.pos[1] + j][self.pos[0]] == ""):
+                moves.append([self.pos[0], self.pos[1] + j])
+                j += 1
+            else:
+                break
+
+        # Left Direction
+        k = 1
+        while True:
+            if (self.pos[0] - k >= 0) and (self.game.board[self.pos[1]][self.pos[0] - k] == ""):
+                moves.append([self.pos[0] - k, self.pos[1]])
+                k += 1
+            else:
+                break
+
+        # # Right Direction
+        m = 1
+        while True:
+            if (self.pos[0] + m <= 7) and (self.game.board[self.pos[1]][self.pos[0] + m] == ""):
+                # print("Ads")
+                moves.append([self.pos[0] + m, self.pos[1]])
+                m += 1
+            else:
+                break
+
+        return moves
